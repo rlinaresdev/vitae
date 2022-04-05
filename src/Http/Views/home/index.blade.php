f3f4ccc67322a524de00019cc35a82d5bb9fa3a7
Ramon A Linares <rlinares4381@gmail.com>
2022-04-05 09:18:31 -0400

    Add Job 0.0.4 Estilos & Iconos - Iniciando layout

--- src/Http/Views/home/index.blade.php
+++ src/Http/Views/home/index.blade.php
@@ -5,13 +5,13 @@
       <title></title>
 
       <link href="{{url('vitae/css/bootstrap.min.css')}}" rel="stylesheet">
-      <link href="{{url('vitae/css/mdi-6.5.95.min.css')}}" rel="stylesheet">
+      <link href="{{url('vitae/css/mdi-6595.min.css')}}" rel="stylesheet">
    </head>
    <body>
       <article class="container">
          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </article>
       <script src="{{url('vitae/js/bootstrap.min.js')}}"></script>
-      <script src="{{url('vitae/js/jquery-3.6.0.min.js')}}"></script>
+      <script src="{{url('vitae/js/jquery-360.min.js')}}"></script>
    </body>
 </html>
