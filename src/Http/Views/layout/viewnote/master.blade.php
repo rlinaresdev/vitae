d116a0a45c40b3a6044d7c7f4d16bf7d755b863f
Ramon A Linares <rlinares4381@gmail.com>
2022-04-07 09:18:05 -0400

    Add Job 0.0.5 Avance layout

--- src/Http/Views/layout/viewnote/master.blade.php
+++ src/Http/Views/layout/viewnote/master.blade.php
@@ -4,12 +4,13 @@
    <head>
       <meta charset="{{$charset}}">
       <title>{{$title}}</title>
+      <!-- CSS only -->
       <link href="{{url('vitae/css/bootstrap.min.css')}}" rel="stylesheet">
       <link href="{{url('vitae/css/mdi-6595.min.css')}}" rel="stylesheet">
       <link href="{{url('vitae/css/layout.ui.css')}}" rel="stylesheet">
    </head>
 
-   <body>
+   <body class="vitae">
 
       <article class="container">
 
@@ -19,15 +20,15 @@
                <a href="{{url("/notes")}}" class="nav-link">Anotaciones</a>
             </div>
 
-            <div class="nav ml-auto">
+            <div class="nav vitae-nav-right ml-auto">
                <div class="nav-item dropdown">
                   <a href="#" class="nav-link dropdown-toggle"
                      id="dropdownTool"
-                     data-toggle="dropdown"
+                     data-bs-toggle="dropdown"
                      aria-expanded="false">
-                     Link
+                     <i class="mdi mdi-dot-net"></i>
                   </a>
-                  <ul class="dropdown-menu" aria-labelledby="dropdownTool">
+                  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownTool">
                      <li>
                         <a href="#" class="dropdown-item">Link</a>
                      </li>
@@ -40,8 +41,8 @@
 
       </article>
 
-      <script src="{{url('vitae/js/bootstrap.min.js')}}"></script>
       <script src="{{url('vitae/js/jquery-360.min.js')}}"></script>
+      <script src="{{url('vitae/js/bootstrap.min.js')}}"></script>
       <script src="{{url('vitae/js/layout.ui.js')}}"></script>
    </body>
 
