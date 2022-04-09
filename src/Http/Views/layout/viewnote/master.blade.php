7ef760d6324f1b7a3f69a975bd23710b75a4590b
Ramon A Linares <rlinares4381@gmail.com>
2022-04-09 08:50:47 -0400

    Add Job 0.0.6.2 Layout & Style

--- src/Http/Views/layout/viewnote/master.blade.php
+++ src/Http/Views/layout/viewnote/master.blade.php
@@ -10,48 +10,54 @@
       <link href="{{url('vitae/css/layout.ui.css')}}" rel="stylesheet">
    </head>
 
-   <body class="vitae">
+   <body>
+      <main role="vitae" class="vitae-layout-lg">
+         <nav class="vitae-navbar">
+            <div class="navbar navbar-light border-bottom">
+               <a href="#" class="navbar-brand">
+                  <i class="mdi mdi-home"></i> Home
+               </a>
 
-      <article class="container">
+               <div class="nav vitae-nav-right ml-auto">
 
-         <nav class="navbar navbar-light border-bottom">
-            <div class="nav">
-               <a href="{{url("/")}}" class="nav-link">Personal</a>
-               <a href="{{url("/notes")}}" class="nav-link">Anotaciones</a>
-            </div>
+                  <a href="{{url("/")}}" class="nav-link">Personal</a>
+                  <a href="{{url("/notes")}}" class="nav-link">Anotaciones</a>
 
-            <div class="nav vitae-nav-right ml-auto">
-               <div class="nav-item dropdown">
-                  <a href="#" class="nav-link dropdown-toggle"
-                     id="dropdownTool"
-                     data-bs-toggle="dropdown"
-                     aria-expanded="false">
-                     <i class="mdi mdi-dot-net"></i>
-                  </a>
-                  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownTool">
-                     <li>
-                        <a href="#" class="dropdown-item">Link</a>
-                     </li>
-                  </ul>
+                  <div class="nav-item dropdown">
+                     <a href="#" class="nav-link dropdown-toggle"
+                        id="dropdownTool"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <i class="mdi mdi-dot-net"></i>
+                     </a>
+                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownTool">
+                        <li>
+                           <a href="#" class="dropdown-item">Link</a>
+                        </li>
+                     </ul>
+                  </div>
                </div>
             </div>
          </nav>
 
-         <section class="row">
-            <article class="col-4">
-               <img src="{{url("vitae/images/linares.png")}}" width="200" alt="@">
-            </article>
-            <article class="col-8">
-               <hgroup>
-                  <h1>Ramon A Linares Febles</h1>
-                  <h4></h4>
-               </hgroup>
-            </article>
-         </section>
+         <article class="vitae-body">
+            <section class="row">
+               <article class="col-4">
+                  <img src="{{url("vitae/images/linares.png")}}" width="180" alt="@">
+               </article>
+               <article class="col-8">
+                  <hgroup>
+                     <h1>Ramon A Linares Febles</h1>
+                     <h4>Full stack Developer </h4>
+                  </hgroup>
+                  <p>
+                     Profesional con más de 10 años de experiencia en el desarrollo de aplicaciones corporativas para grandes y pequeñas empresas.
+                  </p>
+               </article>
+            </section>
+         </article>
 
-         @yield("body")
-
-      </article>
+      </main>
 
       <script src="{{url('vitae/js/jquery-360.min.js')}}"></script>
       <script src="{{url('vitae/js/bootstrap.min.js')}}"></script>
